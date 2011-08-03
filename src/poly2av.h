819bb96807d1ba4ee66abbf990c4ac8f89004d15
Steffen Mueller <smueller@cpan.org>
2011-08-03 09:48:05 +0200

    Make Math::Clipper compile again
    The work of two long commutes!
    
    Missing: Documentation updates
    Missing really badly: Making the actual clipping work again.
    It's not clear whether this is a wrapper or a library issue.

--- src/poly2av.h
+++ src/poly2av.h
@@ -1,10 +1,10 @@
-#ifndef __clipper_poly2av_h_
-#define __clipper_poly2av_h_
+#ifndef clipper_poly2av_h_
+#define clipper_poly2av_h_
 
-#include "clipper.hpp"
+#include "myinit.h"
 
 SV*
-poly2rvav(pTHX_ const clipper::TPolygon& poly)
+polygon2perl(pTHX_ const clipper::Polygon& poly)
 {
   AV* av = newAV();
   AV* innerav;
@@ -22,24 +22,51 @@ poly2rvav(pTHX_ const clipper::TPolygon& poly)
 
 
 SV*
-polypoly2rvav(pTHX_ const clipper::TPolyPolygon& poly)
+polygons2perl(pTHX_ const clipper::Polygons& poly)
 {
   AV* av = newAV();
   SV* innerav;
   const unsigned int len = poly.size();
   av_extend(av, len-1);
   for (unsigned int i = 0; i < len; i++) {
-    innerav = poly2rvav(aTHX_ poly[i]);
+    innerav = polygon2perl(aTHX_ poly[i]);
     av_store(av, i, innerav);
   }
   return (SV*)newRV_noinc((SV*)av);
 }
 
-clipper::TPolygon*
-av2poly(pTHX_ AV* theAv)
+
+SV*
+expolygon2perl(pTHX_ const clipper::ExPolygon& poly)
+{
+  HV* hv = newHV();
+  hv_stores( hv, "outer", (SV*)polygon2perl(aTHX_ poly.outer) );
+  hv_stores( hv, "holes", (SV*)polygons2perl(aTHX_ poly.holes) );
+  return (SV*)newRV_noinc((SV*)hv);
+}
+
+
+SV*
+expolygons2perl(pTHX_ const clipper::ExPolygons& polys)
+{
+  AV* av = newAV();
+  SV* innerav;
+  const unsigned int len = polys.size();
+  av_extend(av, len-1);
+  for (unsigned int i = 0; i < len; i++) {
+    innerav = expolygon2perl(aTHX_ polys[i]);
+    av_store(av, i, innerav);
+  }
+  return (SV*)newRV_noinc((SV*)av);
+}
+
+
+
+clipper::Polygon*
+perl2polygon(pTHX_ AV* theAv)
 {
   const unsigned int len = av_len(theAv)+1;
-  clipper::TPolygon* retval = new clipper::TPolygon(len);
+  clipper::Polygon* retval = new clipper::Polygon(len);
   SV** elem;
   AV* innerav;
   for (unsigned int i = 0; i < len; i++) {
@@ -52,18 +79,19 @@ av2poly(pTHX_ AV* theAv)
       return NULL;
     }
     innerav = (AV*)SvRV(*elem);
-    clipper::TDoublePoint& p = (*retval)[i];
-    p.X = SvNV(*av_fetch(innerav, 0, 0));
-    p.Y = SvNV(*av_fetch(innerav, 1, 0));
+    clipper::IntPoint& p = (*retval)[i];
+    p.X = (clipper::long64)SvIV(*av_fetch(innerav, 0, 0));
+    p.Y = (clipper::long64)SvIV(*av_fetch(innerav, 1, 0));
   }
   return retval;
 }
 
-clipper::TPolyPolygon*
-av2polypoly(pTHX_ AV* theAv)
+
+clipper::Polygons*
+perl2polygons(pTHX_ AV* theAv)
 {
   const unsigned int len = av_len(theAv)+1;
-  clipper::TPolyPolygon* retval = new clipper::TPolyPolygon(len);
+  clipper::Polygons* retval = new clipper::Polygons(len);
   SV** elem;
   AV* innerav;
   for (unsigned int i = 0; i < len; i++) {
@@ -76,7 +104,7 @@ av2polypoly(pTHX_ AV* theAv)
       return NULL;
     }
     innerav = (AV*)SvRV(*elem);
-    clipper::TPolygon* tmp = av2poly(aTHX_ innerav);
+    clipper::Polygon* tmp = perl2polygon(aTHX_ innerav);
     if (tmp == NULL) {
       delete retval;
       return NULL;
@@ -86,4 +114,75 @@ av2polypoly(pTHX_ AV* theAv)
   }
   return retval;
 }
+
+
+#define AV_CHECK(outav, hv, key)                                                       \
+    STMT_START {                                                                       \
+      SV** buf = hv_fetchs(hv, key, 0);                                                \
+      if (!buf || !*buf) {                                                             \
+        croak("Missing ExPolygon hash key: '" key "' or not an array reference.");     \
+      }                                                                                \
+      SvGETMAGIC(*buf);                                                                \
+      if (SvROK(*buf) && SvTYPE(SvRV(*buf)) == SVt_PVAV) {                             \
+        outav = (AV*)SvRV(*buf);                                                       \
+      }                                                                                \
+      else {                                                                           \
+        croak(key " is not an ARRAY reference");                                       \
+      }                                                                                \
+    } STMT_END
+
+clipper::ExPolygon*
+perl2expolygon(pTHX_ HV* theHv)
+{
+  AV* outerav;
+  AV* holesav;
+  AV_CHECK(outerav, theHv, "outer");
+  AV_CHECK(holesav, theHv, "holes");
+
+  clipper::ExPolygon* retval = new clipper::ExPolygon();
+  clipper::Polygon* tmp = perl2polygon(aTHX_ outerav);
+  if (!tmp) {
+    delete retval;
+    return NULL;
+  }
+  retval->outer = *tmp;
+
+  clipper::Polygons* tmps = perl2polygons(aTHX_ holesav);
+  if (!tmps) {
+    delete retval;
+    return NULL;
+  }
+  retval->holes = *tmps;
+
+  return retval;
+}
+#undef AV_CHECK
+
+
+clipper::ExPolygons*
+perl2expolygons(pTHX_ AV* theAv)
+{
+  const unsigned int len = av_len(theAv)+1;
+  std::vector<clipper::ExPolygon> tmpEx; // Done because of croak
+
+  SV** elem;
+  HV* innerhv;
+  for (unsigned int i = 0; i < len; i++) {
+    elem = av_fetch(theAv, i, 0);
+    if (!SvROK(*elem)
+        || SvTYPE(SvRV(*elem)) != SVt_PVHV)
+      return NULL;
+    innerhv = (HV*)SvRV(*elem);
+    clipper::ExPolygon* tmp = perl2expolygon(aTHX_ innerhv);
+    if (tmp == NULL)
+      return NULL;
+    tmpEx[i] = *tmp;
+    delete tmp;
+  }
+
+  clipper::ExPolygons* retval = new clipper::ExPolygons(tmpEx);
+  return retval;
+}
+
+
 #endif
