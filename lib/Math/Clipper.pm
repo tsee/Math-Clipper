ec4334ad751f29097c3ff3d0f0cb6e2d55e01476
Mike Sheldrake <956237+mesheldrake@users.noreply.github.com>
2019-05-11 15:51:47 -0700

    Changes and version bump

--- lib/Math/Clipper.pm
+++ lib/Math/Clipper.pm
@@ -12,7 +12,7 @@ our @ISA = qw(Exporter);
 
 BEGIN {
     use XSLoader;
-    $VERSION = '1.27';
+    $VERSION = '1.28';
     XSLoader::load('Math::Clipper', $VERSION);
 }
 
@@ -634,7 +634,7 @@ The C<Math::Clipper> module is
 
 Copyright (C) 2010, 2011, 2014 by Steffen Mueller
 
-Copyright (C) 2011, 2018 by Mike Sheldrake
+Copyright (C) 2011, 2018, 2019 by Mike Sheldrake
 
 Copyright (C) 2012, 2013 by Alessandro Ranellucci and Mike Sheldrake
 
